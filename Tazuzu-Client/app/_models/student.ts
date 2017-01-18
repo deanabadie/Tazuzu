27703c163efdf6b78ce08832b6dbc845f9b4a4c3
ifateli <ifateli@post.bgu.ac.il>
2017-01-18 23:16:18 +0200

    new client

---
+++ Tazuzu-Client/app/_models/student.ts
@@ -0,0 +1,14 @@
+export class Student {
+    id: number;
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    gender: string;
+    dateOfBirth: Date;
+    email: string;
+    school: string;
+    class: string;
+    height: number;
+    weight: number;
+}
