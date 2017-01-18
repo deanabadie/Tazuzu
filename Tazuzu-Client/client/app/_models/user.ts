41fae2805c18270ae07a9705d8f3757b76133e35
ifateli <ifateli@post.bgu.ac.il>
2017-01-18 22:25:27 +0200

    bla

--- Tazuzu-Client/client/app/_models/user.ts
+++ Tazuzu-Client/client/app/_models/user.ts
@@ -4,4 +4,11 @@
     password: string;
     firstName: string;
     lastName: string;
+    gender: string;
+    dateOfBirth: Date;
+    email: string;
+    school: string;
+    class: string;
+    height: number;
+    weight: number;
 }
