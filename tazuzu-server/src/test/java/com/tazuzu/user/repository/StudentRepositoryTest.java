03d3ae89dfc8802247736a0cfae56d35e5b84850
deanabadie <dean89@gmail.com>
2017-03-13 23:59:42 +0200

    Added tests

--- tazuzu-server/src/test/java/com/tazuzu/user/repository/StudentRepositoryTest.java
+++ tazuzu-server/src/test/java/com/tazuzu/user/repository/StudentRepositoryTest.java
@@ -1,10 +1,33 @@
-//package com.tazuzu.user.repository;
-//
-//import static org.junit.Assert.*;
-//
-///**
-// * Created by Ohad on 19/01/2017.
-// */
-//public class StudentRepositoryTest {
-//
-//}
+package com.tazuzu.user.repository;
+
+/**
+ * Created by deana on 13/03/2017.
+ */
+import com.tazuzu.user.domain.Student;
+import org.junit.*;
+import org.junit.runner.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.*;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+public class StudentRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private StudentRepository repository;
+
+    @Test
+    public void findByUserNameTest() throws Exception {
+        Student s = this.repository.findByUserName("jacky");
+        assertThat(s.getFirstName()).isEqualTo("Jack");
+        assertThat(s.getLastName()).isEqualTo("Sparrow");
+    }
+
+}
